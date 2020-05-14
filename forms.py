a5041dd1771b9fc7305d599bda01ba2af0382307
PALWolfOS <61064672+PALWolfOS@users.noreply.github.com>
2020-05-14 18:42:39 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DateTimeField, IntegerField
 from wtforms.validators import InputRequired, EqualTo, Email
 
 class SignUpForm(FlaskForm):
@@ -13,3 +13,11 @@ class LogInForm(FlaskForm):
     username = StringField('username', validators=[InputRequired()])
     password = PasswordField('New Password', validators=[InputRequired()])
     submit = SubmitField('Login')
+    
+class WorkoutForm(FlaskForm):
+    title = StringField('title', validators=[InputRequired()])
+    duration = IntegerField('Length
+    date = DateTimeField('Date', validators=[InputRequired(), DateRange(
+            min=datetime(2000, 1, 1),
+            max=datetime(2000, 10, 10))]
+    
